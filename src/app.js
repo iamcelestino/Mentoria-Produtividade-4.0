2828f00182eb62f956da39b90374867e5daf2ad3
iamcelestino <trcelestino488@gmail.com>
2024-05-17 16:05:07 -0700

    feat: add dropdown

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const dropdowns = document.querySelectorAll('.dropdown');
 
 dropdowns.forEach(dropdown => {
     dropdown.addEventListener('click',  () => {
+        dropdown.lastElementChild.style.transform ='rotate(90deg)';
         const x = dropdown.nextElementSibling;
         x.classList.toggle('hidden');
     })
